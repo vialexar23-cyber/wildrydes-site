56d6854a4b7522b94d9cca2e8e6bf97a151e485e
vialexar23-cyber <vialexar23@gmail.com>
2025-10-26 00:22:58 -0500

    Update Cognito user pool configuration

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_jro9d8bol', // e.g. us-east-1_uXboG5pAb
-        userPoolClientId: 'a8ddaq79riavimrcus2ouhscr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_46nKkyAnZ', // e.g. us-east-1_uXboG5pAb
+        userPoolClientId: '1dlcahuu5nia0d0o8hcn6dvqqi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-1.amazonaws.com/prod',
     }
 };
+
 
 
 
