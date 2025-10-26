7c8f1076a3851ecf09f427cb7bc83acc263c800d
vialexar23-cyber <vialexar23@gmail.com>
2025-10-25 23:51:47 -0500

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_jro9d8bol', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'a8ddaq79riavimrcus2ouhscr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
