3ef9fcfec4907323325f2f811d69955e66a70c79
vialexar23-cyber <vialexar23@gmail.com>
2025-10-25 23:56:36 -0500

    Update API invokeUrl region in config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_jro9d8bol', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_jro9d8bol', // e.g. us-east-1_uXboG5pAb
         userPoolClientId: 'a8ddaq79riavimrcus2ouhscr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-1.amazonaws.com/prod',
     }
 };
+
 
